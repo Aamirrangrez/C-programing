8cecd18173de570a15b6aa9234019c043c41f7e1
Aamir Rangrez <142010208+Aamirrangrez@users.noreply.github.com>
2023-08-12 22:44:45 +0530

    table.c

---
+++ table.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#define pf printf
+void main(void)
+{
+    int x,y,z;
+    pf("Enter a number to display table of it:");
+    scanf("%d",&z);
+    for(x=1;x<=10;x++)
+    {
+        y=z*x;
+        printf("%dx%d=%d\n",z,x,y);
+
+
+    }
+    fflush(stdin);
+    scanf("%d",&z);
+
+
+}
+
+output: 5
+5x1=5
+5x2=10
+5x3=15
+5x4=20
+5x5=25
+5x6=30
+5x7=35
+5x8=40
+5x9=45
+5x10=50
